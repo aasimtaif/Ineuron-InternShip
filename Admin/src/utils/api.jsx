e3d4524d01836380032483414379def040a0abe6
aasimtaif <taifullahansari@gmail.com>
2024-05-12 02:30:37 +0530

    ...

--- Admin/src/utils/api.jsx
+++ Admin/src/utils/api.jsx
@@ -6,8 +6,6 @@ export const Axios = axios.create({
     baseURL: 'https://ineuron-internship.onrender.com/api/',
     headers: {
         'authorization': `Bearer ${token}`,
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json',
     },
     withCredentials: true
 });
