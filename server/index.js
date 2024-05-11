0f76ffbce91e8ec9ebf7a9111cd42958bf9404d0
aasimtaif <taifullahansari@gmail.com>
2024-05-11 22:53:44 +0530

    .

--- server/index.js
+++ server/index.js
@@ -1,27 +1,21 @@
 import express from "express";
-import authRoute from "./route/auth.js";
-import productRoute from "./route/Products.js";
-import CategoryRoute from "./route/Category.js";
-import cartRoute from "./route/Cart.js";
-import orderRoute from "./route/Order.js";
-import userRoute from "./route/user.js";
+import authRoute from "../route/auth.js";
+import productRoute from "../route/Products.js";
+import CategoryRoute from "../route/Category.js";
+import cartRoute from "../route/Cart.js";
+import orderRoute from "../route/Order.js";
+import userRoute from "../route/user.js";
 import cors from "cors";
-import paymentRoute from "./route/Payment.js"
+import paymentRoute from "../route/Payment.js"
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 const app = express();
 
-const corseConfig = {
-    origin: '',
-    credentials: true,
-    methods: 'GET, POST, PUT, DELETE, OPTIONS',
-}
 
 
 dotenv.config()
 app.use(express.json())
-app.options('', cors(corseConfig))
-app.use(cors(corseConfig))
+app.use(cors())
 
 
 const DataBaseConnection = async () => {
