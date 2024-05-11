abbb16f0f0a54a9b270cd0697a7c15970b1e5fbf
aasimtaif <taifullahansari@gmail.com>
2024-05-11 23:24:23 +0530

    made changes in cores settings

--- server/index.js
+++ server/index.js
@@ -15,12 +15,13 @@ const app = express();
 
 dotenv.config()
 app.use(express.json())
-const corsOption = {
-    origin: ['http://localhost:5174', 'http://localhost:5173'],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-}
-app.use(cors(corsOption))
+app.use(cors())
+app.use(function (req, res, next) { //allow cross origin requests
+    res.setHeader("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", true);
+    next();
+});
 
 
 const DataBaseConnection = async () => {
