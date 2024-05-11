ab8e27a8c8860cbc998f43fec9e4bc2dc53240c4
aasimtaif <taifullahansari@gmail.com>
2024-05-11 21:15:35 +0530

    .

--- server/route/Products.js
+++ server/route/Products.js
@@ -4,17 +4,15 @@ import { verifyUser, verifyAdmin } from '../utils/verify.js';
 
 const router = express.Router();
 router.post('/', verifyAdmin, addProducts)
-router.get('/', (req, res) => {
-    res.send("Welcome to Products Route")
-})
+router.get('/', getProducts)
 router.get('/new-products', getNewProducts)
 router.get('/featured', getFeaturedProducts)
 router.get('/search', searchProducts)
 router.get('/:id', getProduct)
 router.put('/:id', verifyAdmin, updateProduct)
 router.put('/reviews/:id', verifyUser, addProductReview)
-router.delete('/:id', verifyAdmin, deleteProduct)
-router.delete('/image/:id', verifyAdmin, deleteImage)
+router.delete('/:id',verifyAdmin, deleteProduct)
+router.delete('/image/:id',verifyAdmin, deleteImage)
 
 
 export default router;
