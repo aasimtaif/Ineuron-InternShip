50d89373d3b6529710f755abccdd4ebc3d936444
aasimtaif <taifullahansari@gmail.com>
2024-05-11 23:32:41 +0530

    ready to deploy

--- Admin/src/utils/api.jsx
+++ Admin/src/utils/api.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const token = JSON.parse(localStorage.getItem('token')) || null;
 console.log(token)
 export const Axios = axios.create({
-    baseURL: 'http://localhost:8800/api/',
+    baseURL: 'https://ineuron-internship.onrender.com/api/',
     headers: {
         'authorization': `Bearer ${token}`,
     },
