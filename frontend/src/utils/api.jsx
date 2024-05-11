355e24b6346db51b6f85c8a5288605a7788ff148
aasimtaif <taifullahansari@gmail.com>
2024-05-12 02:39:09 +0530

    deploying setting

--- frontend/src/utils/api.jsx
+++ frontend/src/utils/api.jsx
@@ -5,5 +5,8 @@ export const Axios = axios.create({
     baseURL: 'https://ineuron-internship.onrender.com/api/',
     headers: {
         'authorization': `Bearer ${token}`,
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
     },
+    withCredentials: false
 });
