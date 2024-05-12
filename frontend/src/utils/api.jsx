a3ac41206dbb2266a375134e71d37ac762563af6
aasimtaif <taifullahansari@gmail.com>
2024-05-12 20:45:41 +0530

    payment error

--- frontend/src/utils/api.jsx
+++ frontend/src/utils/api.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const token = JSON.parse(localStorage.getItem('token')) || 'none';
 export const Axios = axios.create({
-    baseURL: 'http://localhost:8800/api/',
+    baseURL: 'https://ineuron-internship.onrender.com/api/',
     headers: {
         'authorization': `Bearer ${token}`,
         'Access-Control-Allow-Origin': '*',
